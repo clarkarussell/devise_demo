49fa88c385cebfcf310af88ce934bc0ff9cb223e
Clark Russell <clarkarussell@gmail.com>
2011-05-31 09:56:44 -0700

    Mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ DeviseDemo::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
 	config.action_mailer.raise_delivery_errors = true
 	config.action_mailer.delivery_method = :smtp
-	config.action_mailer.default_url_options = { :host => simple-planet-592.heroku.com' }
+	config.action_mailer.default_url_options = { :host => 'simple-planet-592.heroku.com' }
 	ActionMailer::Base.smtp_settings = {
 		:address    => "smtp.sendgrid.net",
 		:port       => 25,
