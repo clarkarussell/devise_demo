4329edcd8a728490713cd1fef3d8214824ce9177
Clark Russell <clarkarussell@gmail.com>
2011-05-31 09:54:33 -0700

    Mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,17 @@ DeviseDemo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.default_url_options = { :host => simple-planet-592.heroku.com' }
+	ActionMailer::Base.smtp_settings = {
+		:address    => "smtp.sendgrid.net",
+		:port       => 25,
+		:user_name  => ENV['SENDGRID_USERNAME'],
+		:password   => ENV['SENDGRID_PASSWORD'],
+		:domain     => ENV['SENDGRID_DOMAIN'],
+		:authentication  => :plain
+	}
 
   # Enable threaded mode
   # config.threadsafe!
